e08bb711197a996ec26329eb2fa64ce1ff33c71b
FUJI Goro (gfx) <gfuji@cpan.org>
2016-03-08 22:43:54 +0900

    update benchmark

--- example/src/main/java/com/github/gfx/static_gson/example/Session.java
+++ example/src/main/java/com/github/gfx/static_gson/example/Session.java
@@ -4,7 +4,9 @@ import com.bluelinelabs.logansquare.annotation.JsonField;
 import com.bluelinelabs.logansquare.annotation.JsonObject;
 import com.github.gfx.static_gson.annotation.JsonSerializable;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @JsonObject
 @JsonSerializable
@@ -14,17 +16,18 @@ public class Session {
     public int id = 41;
 
     @JsonField
-    public String title = "The Basics of JSON";
+    public String title = "The Basics of JSON processing libraries";
 
     @JsonField
     public String description
             = "How to handle JSON in Android Applications with JSON processing libraries.\n"
             + "There are a lot of JSON processing libraries: org.json, Gson, JsonPullParser, Moshi, LoganSquare\n"
             + "Some uses annotation processing while the other uses reflection.\n"
-            + "blah blah blah blah...";
+            + "If you want a high performance JSON processing library, you should find annotation processing ones,\n"
+            + "because blah blah blah blah...";
 
     @JsonField
-    public long speakerId = 42;
+    public Speaker speaker = new Speaker();
 
     @JsonField
     public long startTime = new Date().getTime();
@@ -33,32 +36,65 @@ public class Session {
     public long endTime = new Date().getTime();
 
     @JsonField
-    public long categoryId = 43;
+    public Category category = new Category();
 
     @JsonField
-    public long placeId = 44;
+    public Room room = new Room();
 
     @JsonField
     public String languageId = "en";
 
     @JsonField
-    public Asset slideUrl = Asset.create("slide", "http://example.com/slide");
-
-    @JsonField
-    public Asset movieUrl = Asset.create("movie", "http://example.com/movie");
-
-    @JsonField
-    public Asset shareUrl = Asset.create("share", "http://example.com/share");
+    public List<Asset> assets = Arrays.asList(
+            Asset.create("slide", "http://example.com/slide"),
+            Asset.create("movie", "http://example.com/movie"),
+            Asset.create("share", "http://example.com/share")
+    );
 
     @JsonField
     public boolean checked = false;
 
-    @JsonObject
     @JsonSerializable
+    @JsonObject
+    public static class Speaker {
+
+        @JsonField
+        public long id = 42;
+
+        @JsonField
+        public String name = "Foo Bar Baz";
+    }
+
+    @JsonSerializable
+    @JsonObject
+    public static class Category {
+
+        @JsonField
+        public long id = 43;
+
+        @JsonField
+        public String name = "JSON";
+    }
+
+    @JsonSerializable
+    @JsonObject
+    public static class Room {
+
+        @JsonField
+        public long id = 44;
+
+        @JsonField
+        public String name = "Room A";
+    }
+
+    @JsonSerializable
+    @JsonObject
     public static class Asset {
 
+        @JsonField
         public String label;
 
+        @JsonField
         public String url;
 
         public static Asset create(String label, String url) {
